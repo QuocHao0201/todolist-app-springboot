c068008ddfdd7b2ae9849e54ebb5b8a2a431be2e
QuocHao0201 <huynhquochao0201@gmail.com>
2025-04-10 01:06:34 +0700

    cập nhật code todo list lên nhánh hao

---
+++ src/main/java/com/hao/todo/todoapp/service/TaskService.java
@@ -0,0 +1,47 @@
+package com.hao.todo.todoapp.service;
+
+import com.hao.todo.todoapp.entity.Task;
+import com.hao.todo.todoapp.repository.TaskRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks(){
+        return  taskRepository.findAll();
+    }
+
+    public Task getTaskByID(int id){
+        return taskRepository.findById(id).orElseThrow(()->new RuntimeException("Task not found by id:"+id ));
+    }
+
+    public Task CreateTask(Task task){
+        task.setCreatedAt(LocalDateTime.now());
+        task.setStatus(Task.Status.pending);
+    return taskRepository.save(task);
+    }
+
+    public Task UpdateTask(int id, Task taskDetails){
+        Task task = getTaskByID(id);
+
+        task.setTitle(taskDetails.getTitle());
+        task.setDescription(taskDetails.getDescription());
+        task.setDueDate(taskDetails.getDueDate());
+        task.setStatus(taskDetails.getStatus());
+        return taskRepository.save(task);
+    }
+
+    public void DeleteTask(int id){
+        Task task = getTaskByID(id);
+        taskRepository.delete(task);
+    }
+
+}
